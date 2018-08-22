3a910f624cd91547d4a3e24ea9adf5a967acc909
Dan Wright <dan.wright@platform9.com>
2018-08-22 22:41:09 +0000

    update branch

--- roles/wait-for-convergence/files/wait_for_agent_convergence.sh
+++ roles/wait-for-convergence/files/wait_for_agent_convergence.sh
@@ -52,12 +52,10 @@ elapsedTime=0
 while [ ${elapsedTime} -lt ${TIMEOUT} ]; do
   role_status=$(curl -k -H "Content-Type: application/json" -H "X-Auth-Token: ${token}" \
       https://${ctrl_ip}/resmgr/v1/hosts/${host_id} 2>/dev/null | python -m json.tool | grep role_status)
-  echo "--> pre.role_status= [${role_status}]"
   if [ -n "${role_status}" ]; then
     role_status=$(echo ${role_status} | cut -d : -f2 | sed -e 's/\"//g' | sed -e 's/,//g' | sed -e 's/ //g')
   fi
 
-  echo "--> parsed.role_status= [${role_status}]"
   if [ "${role_status}" == "ok" ]; then break; fi
 
   # update elapsed time
