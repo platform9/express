323948197100019cdcfcdd1d13bf61fbfebb60b1
Dan Wright <dan.wright@platform9.com>
2018-09-24 17:37:30 +0000

    integrate pf9-builder with ci build-script

--- lib/ci/build-script.sh
+++ lib/ci/build-script.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-echo "Starting: PF9-Express CI Build Script"
+echo "############################################################"
+echo "### RUNNING - PF9-Express CI Build Script"
+echo "############################################################"
 echo "--> Date   : $(date -u)"
 echo "--> Branch : Branch: $(git branch | grep ^*)"
+echo -e "--> Directory : $(pwd)\n"
+
+# create OpenStack instances and run pf9-express
+./lib/pf9-builder/pf9-builder --nova lib/ci/kvm.centos.cfe.csv lib/ci/cfe.rc
+
+# run validation tests (pf9-snitch)
+
+# cleanup
+
+# exit
+exit 0
