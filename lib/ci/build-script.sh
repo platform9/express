3a163e55acab64c5c404b0ef2bed1b6da7307d87
Dan Wright <dan.wright@platform9.com>
2018-09-24 17:51:40 +0000

    m1

--- lib/ci/build-script.sh
+++ lib/ci/build-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # globals
-exec_pf9_builder="./lib/pf9-builder/pf9-builder --nova lib/ci/kvm.centos.cfe.csv lib/ci/cfe.rc"
+exec_pf9_builder="./lib/pf9-builder/pf9-builder --nova -d lib/ci/kvm.centos.cfe.csv lib/ci/cfe.rc"
 
 echo "############################################################"
 echo "### RUNNING - PF9-Express CI Build Script"
