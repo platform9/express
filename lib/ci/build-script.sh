d4c9f5d2b8cc70524c065f915a1944062edb33b4
Dan Wright <dan.wright@platform9.com>
2018-09-24 17:48:27 +0000

    m1

--- lib/ci/build-script.sh
+++ lib/ci/build-script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# globals
+exec_pf9_builder="./lib/pf9-builder/pf9-builder --nova lib/ci/kvm.centos.cfe.csv lib/ci/cfe.rc"
+
 echo "############################################################"
 echo "### RUNNING - PF9-Express CI Build Script"
 echo "############################################################"
@@ -8,7 +11,8 @@ echo "--> Branch : Branch: $(git branch | grep ^*)"
 echo -e "--> Directory : $(pwd)\n"
 
 # create OpenStack instances and run pf9-express
-./lib/pf9-builder/pf9-builder --nova lib/ci/kvm.centos.cfe.csv lib/ci/cfe.rc
+echo "exec: ${exec_pf9_builder}"
+eval ${exec_pf9_builder}
 
 # run validation tests (pf9-snitch)
 
