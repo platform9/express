2230e600e357a59ca8ca71caa5ecc5904cccd479
Dan Wright <dan.wright@platform9.com>
2018-11-21 05:59:03 +0000

    added metrics database per instance

--- lib/tools/pf9-hostmon/pf9-hostmon.py
+++ lib/tools/pf9-hostmon/pf9-hostmon.py
@@ -4,6 +4,8 @@ import sys
 import os.path
 import json
 import signal
+from datetime import datetime
+import calendar
 
 def usage():
     print "usage: {} [<hostname_filter>]".format(sys.argv[0]) 
@@ -17,7 +19,7 @@ def lassert(m):
 def sigint_handler(signum, frame):
     None
 
-def get_peak_cpu(instance_id):
+def write_instance_metrics_to_db(last_timestamp,instance_id,instance_name,instance_flavor,instance_db):
     json_results_file = "/tmp/pf9-hostmon-gnocchi.tmp.json"
     os.system("gnocchi measures show -f json --resource-id {} cpu_util > {}".format(instance_id,json_results_file))
 
@@ -28,22 +30,24 @@ def get_peak_cpu(instance_id):
     # process ceilometer metric: cpu_util
     try:
         json_data=open(json_results_file)
-        data = json.load(json_data)
+        metrics = json.load(json_data)
         json_data.close()
     except:
         lassert("failed to process <json_results_file>")
     else:
-        max_cpu = max_ts = 0
-        for sample_data in data:
+        for metric_data in metrics:
             try:
-                sample_data['value']
+                metric_data['value']
             except:
                 continue
             else:
-                if sample_data['value'] > max_cpu:
-                    max_cpu = sample_data['value']
-                    max_ts = sample_data['timestamp']
-    return max_cpu, max_ts
+                if last_timestamp == 0:
+                    instance_db.write("{},{},{},{}\r\n".format(metric_data['timestamp'],instance_name,instance_flavor,metric_data['value']))
+                else:
+                    last_ts = calendar.timegm(datetime.strptime(last_timestamp, "%Y-%m-%dT%H:%M:%S.%f").timetuple())
+                    currnt_ts = calendar.timegm(datetime.strptime(metric_data['timestamp'], "%Y-%m-%dT%H:%M:%S.%f").timetuple())
+                    if current_ts > last_ts:
+                        instance_db.write("{},{},{},{}\r\n".format(metric_data['timestamp'],instance_name,instance_flavor,metric_data['value']))
 
 # print usage
 if len(sys.argv) == 2 and sys.argv[1] == "-h":
@@ -55,7 +59,7 @@ if len(sys.argv) == 2:
 
 # get list of server IDs
 json_results_file = "/tmp/pf9-hostmon-servers.tmp.json"
-os.system("openstack server list --all-projects -f json > {}".format(json_results_file))
+os.system("openstack server list -f json > {}".format(json_results_file))
 
 # validate json_results_file
 if not os.path.isfile(json_results_file):
@@ -64,19 +68,25 @@ if not os.path.isfile(json_results_file):
 # process server IDs (get ceilometer stats and process)
 try:
     json_data=open(json_results_file)
-    data = json.load(json_data)
+    instances = json.load(json_data)
     json_data.close()
 except:
     lassert("failed to process <json_results_file>")
 else:
-    for sample_data in data:
+    for instance_data in instances:
         try:
-            sample_data['Name']
+            instance_data['Name']
         except:
             continue
         else:
-            max_cpu, max_ts = get_peak_cpu(sample_data['ID'])
-            print "{},{},{},{}".format(sample_data['Name'],sample_data['Flavor'],max_cpu,max_ts)
+            instance_db_fh = open("instance_data/{}".format(instance_data['Name']), "w+")
+            metric_lines = instance_db_fh.readlines()
+            if len(metric_lines) > 0:
+                last_timestamp = metric_lines[len(metric_lines)-1]
+            else:
+                last_timestamp = 0
+
+            write_instance_metrics_to_db(last_timestamp,instance_data['ID'],instance_data['Name'],instance_data['Flavor'],instance_db_fh)
 
 # exit
 sys.exit(0)
