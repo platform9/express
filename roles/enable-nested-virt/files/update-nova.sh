212e37873289eddaa32edc5ab975a9fcb29ea36f
Matt McCarrell <mmccarre@platform9.com>
2020-04-23 15:42:04 -0400

    Updating based on feedback

--- roles/enable-nested-virt/files/update-nova.sh
+++ roles/enable-nested-virt/files/update-nova.sh
@@ -8,9 +8,6 @@ assert() {
 }
 
 echo "[libvirt]" > $NOVA_CONF
-if [ $? -ne 0 ]; then assert "failed to update ${NOVA_CONF}"; fi
-echo "virt_type = qemu" >> $NOVA_CONF
-echo "[libvirt]" > $NOVA_CONF
 echo "libvirt_type = qemu" >> $NOVA_CONF
 
 service pf9-ostackhost restart
